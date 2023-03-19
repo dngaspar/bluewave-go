c2fef87c39ebb45799d4d7f82a6385f51a83f3a0
dngaspar <dngaspar5@gmail.com>
2023-03-19 00:54:55 -0400

    add pdftoimage pdftotext pdftohtml metods

--- main.go
+++ main.go
@@ -2,35 +2,31 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/jpeg"
-	"io/ioutil"
+
+	// "io/ioutil"
 	"os"
 	"path/filepath"
 
 	"github.com/gen2brain/go-fitz"
 )
 
-func main() {
-	doc, err := fitz.New("test.pdf")
+func pdfToImage(filePath string, savePath string, fromPageNumber int, toPageNumber int) []image.Image {
+	doc, err := fitz.New(filePath)
 	if err != nil {
 		panic(err)
 	}
-
+	// len := toPageNumber - fromPageNumber
+	var result []image.Image
 	defer doc.Close()
-
-	tmpDir, err := ioutil.TempDir(os.TempDir(), "fitz")
-	if err != nil {
-		panic(err)
-	}
-
-	// Extract pages as images
-	for n := 0; n < doc.NumPage(); n++ {
+	for n := fromPageNumber - 1; n < toPageNumber; n++ {
 		img, err := doc.Image(n)
 		if err != nil {
 			panic(err)
 		}
-
-		f, err := os.Create(filepath.Join(tmpDir, fmt.Sprintf("test%03d.jpg", n)))
+		// fmt.Printf("img: %s\n", reflect.TypeOf(img))
+		f, err := os.Create(filepath.Join(savePath, fmt.Sprintf("page%03d.jpg", n)))
 		if err != nil {
 			panic(err)
 		}
@@ -41,36 +37,56 @@ func main() {
 		}
 
 		f.Close()
+		result = append(result, img)
+		// result[n] = img
 	}
+	// fmt.Println(result)
+	return result
+}
 
-	// Extract pages as text
-	for n := 0; n < doc.NumPage(); n++ {
+func pdfToText(filePath string, savePath string, fromPageNumber int, toPageNumber int) []string {
+	doc, err := fitz.New(filePath)
+	if err != nil {
+		panic(err)
+	}
+	var result []string
+	defer doc.Close()
+	for n := fromPageNumber - 1; n < toPageNumber; n++ {
 		text, err := doc.Text(n)
 		if err != nil {
 			panic(err)
 		}
-
-		f, err := os.Create(filepath.Join(tmpDir, fmt.Sprintf("test%03d.txt", n)))
+		fmt.Println(text)
+		f, err := os.Create(filepath.Join(savePath, fmt.Sprintf("page%03d.text", n)))
 		if err != nil {
 			panic(err)
 		}
-
 		_, err = f.WriteString(text)
 		if err != nil {
 			panic(err)
 		}
-
+		result = append(result, text)
 		f.Close()
 	}
+	return result
+}
 
-	// Extract pages as html
-	for n := 0; n < doc.NumPage(); n++ {
+func pdfToHtml(filePath string, savePath string, fromPageNumber int, toPageNumber int) []string {
+	doc, err := fitz.New(filePath)
+	if err != nil {
+		panic(err)
+	}
+	var result []string
+	defer doc.Close()
+	for n := fromPageNumber - 1; n < toPageNumber; n++ {
 		html, err := doc.HTML(n, true)
+		// fmt.Printf("html: %s\n", reflect.TypeOf(html))
+		// fmt.Println()
 		if err != nil {
 			panic(err)
 		}
 
-		f, err := os.Create(filepath.Join(tmpDir, fmt.Sprintf("test%03d.html", n)))
+		f, err := os.Create(filepath.Join(savePath, fmt.Sprintf("test%03d.html", n)))
 		if err != nil {
 			panic(err)
 		}
@@ -79,7 +95,16 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-
+		result = append(result, html)
 		f.Close()
 	}
+	return result
+}
+func main() {
+	// doc, err := fitz.New("test.pdf")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	result := pdfToHtml("test.pdf", "assets/html", 1, 4)
+	fmt.Println(result)
 }
