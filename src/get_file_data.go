ebbcfe5ee9ccdb73705088fca60dfe9371ff73e6
dngaspar <dngaspar5@gmail.com>
2023-03-20 09:45:39 -0400

    change get_file_data

--- src/get_file_data.go
+++ src/get_file_data.go
@@ -1,15 +1,17 @@
 package src
 
 import (
+	"bluewave/pdf"
 	"encoding/json"
 	"fmt"
+	"image"
 	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type CacheDataMain struct {
+type Block struct {
 	Text           string    `json:"text"`
 	Digits         string    `json:"digits"`
 	Cum_len_text   int       `json:"cum_len_text"`
@@ -19,15 +21,28 @@ type CacheDataMain struct {
 	Block_num      int       `json:"block_num"`
 }
 
-type CacheData struct {
-	Blocks      []CacheDataMain `json:blocks`
-	Image_hashs []string        `json:image_hashs`
-	N_pages     int             `json:n_pages`
+type ImageHash struct {
 }
 
-type Cache struct {
-	Version string    `json:"version"`
-	Data    CacheData `json:"data"`
+type PdfInfo struct {
+	Blocks      []Block     `json:blocks`
+	Image_hashs []ImageHash `json:image_hashs`
+	N_pages     int         `json:n_pages`
+}
+
+type Pdf struct {
+	Version string  `json:"version"`
+	Data    PdfInfo `json:"data"`
+}
+
+func GetMinMax(a []int) int {
+	max := a[0]
+	for _, value := range a {
+		if value > max {
+			max = value
+		}
+	}
+	return max
 }
 
 func isCompatible(vCurrent string, vCache string) bool {
@@ -42,10 +57,65 @@ func isCompatible(vCurrent string, vCache string) bool {
 
 }
 
+func getPageBlockAndHashes(fileName string, pageNum int) {
+	// """Get page block and hashes"""
+	// var imageHashes []ImageHash
+	// var textBlocks []Block
+	var pageImage image.Image
+	pageImage = pdf.GetPageInfo(fileName, pageNum)
+	// min := pageImage.Bounds().Min
+	max := pageImage.Bounds().Max
+	fmt.Println(max)
+	r, g, b, a := pageImage.At(3, 4).RGBA()
+	fmt.Println(max, r, g, b, a)
+	maxX := max.X
+	maxY := max.Y
+	var blockarr [][]int
+	var white uint32 = 65535
+	var blockNo int
+	var xarr []int
+	for y := 0; y < maxY; y++ {
+		var ok int = 0
+		// var firstline int = 0
+		var len int = 0
+		for x := 0; x < maxX; x++ {
+			r, g, b, a := pageImage.At(x, y).RGBA()
+			if r != white || g != white || b != white || a != white {
+				ok = 1
+				// if firstline == 0 {
+				// 	firstline = 1
+				// }
+				xarr = append(xarr, x)
+				len = x
+			}
+		}
+		if ok == 0 {
+			// if firstline == 1 {
+			for i := 0; i < 4; i++ {
+				blockarr[blockNo][i] = 0
+			}
+			blockarr[blockNo][1] = y
+			if blockNo > 0 {
+				blockarr[blockNo-1][3] = y - blockarr[blockNo-1][1]
+			}
+			// firstline = 0
+			blockarr[blockNo][0] = GetMinMax(xarr)
+			blockarr[blockNo][2] = len
+			xarr = nil
+			blockNo++
+			// }
+		}
+	}
+	// x := pageImage.At(2549, 3299)
+	fmt.Println(blockarr)
+
+}
+
 func readBlocksAndHeaders(fileName string) {
 	if fileName[len(fileName)-4:] != ".pdf" {
-
+		panic("Fitz cannot read non-PDF file")
 	}
+	getPageBlockAndHashes(fileName, 0)
 }
 
 func GetFileData(fullFileName string, fileIndex int, regenCache bool, version string) {
@@ -60,11 +130,11 @@ func GetFileData(fullFileName string, fileIndex int, regenCache bool, version st
 		byteValue, _ := ioutil.ReadAll(cachedFile)
 
 		// var cached Cache
-		var cached Cache
+		var cached Pdf
 		json.Unmarshal(byteValue, &cached)
 		// fmt.Println(cached)
-		var blocks []CacheDataMain
-		var imageHashes []string
+		var blocks []Block
+		var imageHashes []ImageHash
 		var nPages int
 		lenBlocks := 0
 		lenImageHashes := 0
@@ -81,8 +151,8 @@ func GetFileData(fullFileName string, fileIndex int, regenCache bool, version st
 
 		// }
 		if lenBlocks == 0 && lenImageHashes == 0 && nPages == 0 {
-			readBlocksAndHeaders(fullFileName)
 		}
 	}
+	readBlocksAndHeaders(fullFileName)
 
 }
