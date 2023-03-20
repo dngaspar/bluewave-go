40924f051a546839d370aa57772623c0d7d9a1a2
dngaspar <dngaspar5@gmail.com>
2023-03-20 14:20:08 -0400

    start compare_pdf

--- cmd/compare_pdfs/main.go
+++ cmd/compare_pdfs/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/akamensky/argparse"
+)
 
 func main() {
-	fmt.Println("compare_pdfs")
+	parser := argparse.NewParser("Input", "Input args")
+
+	fileName := parser.String("f", "filenames", &argparse.Options{Required: true, Help: "PDF filenames to compare"})
+
+	method := parser.String("m", "methods", &argparse.Options{Required: true, Help: "Which of the three comparison methods to use: text, digits, images"})
+
+	prettyPrint := parser.String("p", "pretty_print", &argparse.Options{Required: true, Help: "Pretty print output"})
+
+	regenCache := parser.String("c", "regen_cache", &argparse.Options{
+		Required: true,
+		Help:     "Ignore and overwrite cached data",
+	})
+
+	sidecarOnly := parser.String("n", "no_importance", &argparse.Options{
+		Required: true,
+		Help:     "Do not generate importance scores",
+	})
+
+	verbose := parser.String("v", "verbose", &argparse.Options{
+		Required: true,
+		Help:     "Print things while running",
+	})
+
+	version := parser.String("vs", "version", &argparse.Options{
+		Required: true,
+		Help:     "Print version",
+	})
+
+	// Parser input
+	err := parser.Parse(os.Args)
+
+	if err != nil {
+		// In case of error print error and print usage
+		// This can also be done by passing -h or --help flags
+		fmt.Println(parser.Usage(err))
+	}
+	fmt.Println(fileName, method, prettyPrint, regenCache, sidecarOnly, verbose, version)
 }
